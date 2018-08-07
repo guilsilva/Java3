df48ba04a6eac94700ff015557953a0265f1f606
Guilherme Lima <33007095+guilsilva@users.noreply.github.com>
2018-08-07 14:00:51 -0300

    bytebank-herdado-funcionario

--- Cliente.java
+++ Cliente.java
@@ -1,25 +1,20 @@
 
-public class Cliente {
-	private String nome;
-	private String cpf;
-	private String profissao;
+public class Cliente implements Autenticavel {
+
+	private AutenticacaoUtil autenticador;
+		
+	public Cliente() {
+		this.autenticador = new AutenticacaoUtil();
+	}
 	
-	public String getNome() {
-		return nome;
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
 	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getProfissao() {
-		return profissao;
-	}
-	public void setProfissao(String profissao) {
-		this.profissao = profissao;
-	}
+
 }
